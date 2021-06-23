93278af9a058c22857b5abfdb3b28f8d66881e3d
Chad Bailey <chadbailey904@gmail.com>
2021-06-22 20:16:46 -0500

    Added mkdocs (not yet usable...)

---
+++ selfdrive/__init__.py
@@ -0,0 +1,8 @@
+"""
+# Module: Selfdrive
+
+`openpilot.selfdrive` is where the core assets of openpilot reside.
+
+`openpilot.selfdrive.manager.manager.main()` is the main loop of openpilot 
+that's ultimately invoked when `launch_openpilot.sh` is called.
+"""
---
+++ selfdrive/manager/__init__.py
@@ -0,0 +1,10 @@
+"""
+OpenPilot Manager
+
+`openpilot.selfdrive.manager.manager` is the primary entrypoint of OpenPilot.
+
+The OpenPilot Manager is responsible for launching and managing threads which
+collectively make up the openpilot application ran on NEOS.
+
+
+"""
